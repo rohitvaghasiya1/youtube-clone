9074ad7c0f493ca9da37226fa0f799a98c52819f
Rohit Vaghasiya <rohit.vaghasiya1@gmail.com>
2024-03-26 15:55:47 +0530

    feat: make common component for search

---
+++ src/components/system/SearchBox.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="YL__toolbar-input-container header-search-bar row no-wrap">
+    <div class="col">
+      <q-input
+        rounded
+        outlined
+        v-model="search"
+        placeholder="Search"
+        @focus="showSearchIcon = true"
+        @blur="showSearchIcon = false"
+        class="col"
+      >
+        <template v-if="showSearchIcon" v-slot:prepend>
+          <q-icon name="search" />
+        </template>
+        <template v-slot:append>
+          <q-icon name="keyboard" class="q-mr-md" />
+          <q-btn flat color="grey-3" text-color="grey-8" icon="search" />
+        </template>
+      </q-input>
+    </div>
+    <div class="col-1 text-right">
+      <q-btn flat round color="grey-3" icon="mic" text-color="grey-8" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+defineOptions({
+  name: "SearchBox",
+});
+
+const search = ref("");
+const showSearchIcon = ref(false);
+
+const props = defineProps({
+  video: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+<style lang="scss">
+.header-search-bar {
+  max-width: 732px;
+  margin: auto;
+  width: 100%;
+  @media screen and (max-width: 1439px) {
+    max-width: 600px;
+  }
+  .q-field__control {
+    height: 40px;
+    padding-right: 0;
+    .q-field__native {
+      padding: 0;
+    }
+    .q-field__marginal {
+      height: 40px;
+      .q-btn-item {
+        border-left: 1px solid rgba(0, 0, 0, 0.24);
+        width: 68px;
+        border-radius: 0 50px 50px 0;
+        height: 40px;
+      }
+    }
+  }
+}
+</style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -20,33 +20,7 @@
 
         <q-space />
 
-        <div
-          v-if="$q.screen.gt.sm"
-          class="YL__toolbar-input-container header-search-bar row no-wrap"
-        >
-          <div class="col">
-            <q-input
-              rounded
-              outlined
-              v-model="search"
-              placeholder="Search"
-              @focus="showSearchIcon = true"
-              @blur="showSearchIcon = false"
-              class="col"
-            >
-              <template v-if="showSearchIcon" v-slot:prepend>
-                <q-icon name="search" />
-              </template>
-              <template v-slot:append>
-                <q-icon name="keyboard" class="q-mr-md" />
-                <q-btn flat color="grey-3" text-color="grey-8" icon="search" />
-              </template>
-            </q-input>
-          </div>
-          <div class="col-1 text-right">
-            <q-btn flat round color="grey-3" icon="mic" text-color="grey-8" />
-          </div>
-        </div>
+        <SearchBox v-if="$q.screen.gt.sm" />
 
         <q-space />
 
@@ -161,14 +135,14 @@
 <script>
 import { ref } from "vue";
 import { fabYoutube } from "@quasar/extras/fontawesome-v6";
+import SearchBox from "../components/system/SearchBox.vue";
 
 export default {
   name: "MainLayout",
-
+  components: {
+    SearchBox,
+  },
   setup() {
-    const search = ref("");
-    const showSearchIcon = ref(false);
-
     const miniState = ref(false);
 
     return {
@@ -188,8 +162,6 @@ export default {
         }
       },
       fabYoutube,
-      search,
-      showSearchIcon,
 
       links1: [
         { icon: "home", text: "Home" },
@@ -247,31 +219,4 @@ export default {
 
     &:hover
       color: #000
-</style>
-
-<style lang="scss">
-.header-search-bar {
-  max-width: 732px;
-  margin: auto;
-  width: 100%;
-  @media screen and (max-width: 1439px) {
-    max-width: 600px;
-  }
-  .q-field__control {
-    height: 40px;
-    padding-right: 0;
-    .q-field__native {
-      padding: 0;
-    }
-    .q-field__marginal {
-      height: 40px;
-      .q-btn-item {
-        border-left: 1px solid rgba(0, 0, 0, 0.24);
-        width: 68px;
-        border-radius: 0 50px 50px 0;
-        height: 40px;
-      }
-    }
-  }
-}
 </style>
